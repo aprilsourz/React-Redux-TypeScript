6ddfe1cb3af8e649a3ccd872b1a1dd3d613f2008
arlofeirman <arlofeirman@gmail.com>
2018-08-12 12:57:23 -0400

    Todo reducer

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,5 +1,39 @@
 import Todo from 'models/Todo'
+import { Action } from 'actions/Action'
+import { ActionTypes } from 'actions/todos'
 
 export interface State {
   todos: Todo[]
+}
+
+export function reducer(state: State = initialState, action: Action) {
+
+  switch (action.type) {
+    case ActionTypes.ADD_TODO: {
+
+      const todo = action.payload.todo
+
+      return {
+        ...state,
+        todos: [...state.todos, todo]
+      }
+    }
+
+    case ActionTypes.TOGGLE_TODO: {
+
+      const { todoId } = action.payload
+
+      return {
+        ...state,
+        todos: state.todos.map(todo => todo.id === todoId ? { ...todo, done: !todo.done } : todo)
+      }
+    }
+
+    default:
+      return state
+
+  }
+
+
+
 }
