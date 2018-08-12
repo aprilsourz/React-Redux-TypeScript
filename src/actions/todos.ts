a412871525ac1ee9f5cb512ee7ec75aded46e7cf
arlofeirman <arlofeirman@gmail.com>
2018-08-11 22:43:17 -0400

    Action types and creators

--- src/actions/todos.ts
+++ src/actions/todos.ts
@@ -2,3 +2,24 @@ export enum ActionTypes {
   ADD_TODO = 'ADD_TODO',
   TOGGLE_TODO = 'TOGGLE_TODO'
 }
+
+export interface AddTodoAction { type: ActionTypes.ADD_TODO, payload: { todo: object } }
+export interface ToggleTodoAction { type: ActionTypes.TOGGLE_TODO, payload: { todoId: number } }
+
+export function addTodo(name: string): AddTodoAction {
+  return {
+    type: ActionTypes.ADD_TODO,
+    payload: {
+      todo: {
+        name: name,
+        done: false
+      }
+    }
+  }
+}
+
+export function toggleTodo(todoId: number): ToggleTodoAction {
+  return { type: ActionTypes.TOGGLE_TODO, payload: { todoId } }
+}
+
+export type Action = ToggleTodoAction | AddTodoAction
