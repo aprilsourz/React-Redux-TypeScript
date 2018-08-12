434b4476086df0cebdc4e5c46ba2ea326567e203
arlofeirman <arlofeirman@gmail.com>
2018-08-11 22:48:48 -0400

    Create state interface for reducer

--- src/actions/todos.ts
+++ src/actions/todos.ts
@@ -1,9 +1,11 @@
+import Todo from 'models/Todo'
+
 export enum ActionTypes {
   ADD_TODO = 'ADD_TODO',
   TOGGLE_TODO = 'TOGGLE_TODO'
 }
 
-export interface AddTodoAction { type: ActionTypes.ADD_TODO, payload: { todo: object } }
+export interface AddTodoAction { type: ActionTypes.ADD_TODO, payload: { todo: Todo } }
 export interface ToggleTodoAction { type: ActionTypes.TOGGLE_TODO, payload: { todoId: number } }
 
 export function addTodo(name: string): AddTodoAction {
