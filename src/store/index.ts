24febc308effccd7a4fef539d566576ba03e4ec7
arlofeirman <arlofeirman@gmail.com>
2018-08-12 14:49:49 -0400

    Add types to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import { createStore } from 'redux'
 import { State, reducer } from '../reducers'
 
-const store = createStore<State>(reducer)
+const store = createStore<State, any, any, any>(reducer)
 
 export default store
