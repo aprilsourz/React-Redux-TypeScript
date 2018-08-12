39b09bbcd8934293cd7ba1b0cf43b397b48ebf05
arlofeirman <arlofeirman@gmail.com>
2018-08-12 15:57:45 -0400

    Tests are working

---
+++ test/components/App.spec.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import App from '../../src/components/App'
+
+
+
+describe('<App />', () => {
+  it('Should render', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).to.be.ok
+  })
+
+})
