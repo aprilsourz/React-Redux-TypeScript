7b2415c4518f0190cb0e3f7bcfefced736053d2e
arlofeirman <arlofeirman@gmail.com>
2018-08-12 21:35:16 -0400

    ADD_TODO reducer test

--- test/reducers/todos.spec.tsx
+++ test/reducers/todos.spec.tsx
@@ -1,4 +1,5 @@
 import { reducer, initialState } from '../../src/reducers/todos'
+import { ActionTypes, addTodo } from '../../src/actions/todos'
 import * as Actions from '../../src/actions/todos'
 import { expect } from 'chai'
 
@@ -8,6 +9,26 @@ import { expect } from 'chai'
 describe('<App />', () => {
   it('should return the initial state', () => {
     expect(reducer(undefined, { payload: {}, type: 'init' })).deep.equal(initialState)
+
+  })
+
+  it('should handle action ADD_TODO', () => {
+    const action = {
+      type: ActionTypes.ADD_TODO,
+      payload: {
+        todo: {
+          name: 'Groceries',
+          done: false,
+          id: 1
+        }
+      }
+    }
+    const expectedState = {
+      todos: [
+        { name: 'Groceries', done: false, id: 1 }
+      ]
+    }
+    expect(reducer({ todos: [] }, addTodo('Groceries'))).deep.equal(expectedState)
 
   })
 
