fc028ec6bab3c22dcb0dc03bb3d2778d48accc3b
arlofeirman <arlofeirman@gmail.com>
2018-08-12 21:50:54 -0400

    Reducer TOGGLE_TODO test

--- test/reducers/todos.spec.tsx
+++ test/reducers/todos.spec.tsx
@@ -1,5 +1,5 @@
 import { reducer, initialState } from '../../src/reducers/todos'
-import { ActionTypes, addTodo } from '../../src/actions/todos'
+import { ActionTypes, addTodo, toggleTodo } from '../../src/actions/todos'
 import * as Actions from '../../src/actions/todos'
 import { expect } from 'chai'
 
@@ -13,23 +13,32 @@ describe('<App />', () => {
   })
 
   it('should handle action ADD_TODO', () => {
-    const action = {
-      type: ActionTypes.ADD_TODO,
-      payload: {
-        todo: {
-          name: 'Groceries',
-          done: false,
-          id: 1
-        }
-      }
-    }
     const expectedState = {
       todos: [
         { name: 'Groceries', done: false, id: 1 }
       ]
     }
     expect(reducer({ todos: [] }, addTodo('Groceries'))).deep.equal(expectedState)
-
   })
 
+  it('should handle action TOGGLE_TODO', () => {
+    const initState = {
+      todos: [
+        { name: 'Mow lawn', id: 1, done: false },
+        { name: 'Buy milk', id: 2, done: false },
+        { name: 'Groceries', id: 3, done: false },
+      ]
+    }
+
+    const expectedState = {
+      todos: [
+        { name: 'Mow lawn', id: 1, done: false },
+        { name: 'Buy milk', id: 2, done: true },
+        { name: 'Groceries', id: 3, done: false },
+      ]
+    }
+
+    expect(reducer(initState, toggleTodo(2))).deep.equal(expectedState)
+
+  })
 })
