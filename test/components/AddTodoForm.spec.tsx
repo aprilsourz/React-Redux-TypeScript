41d7c363af0536627c0916803052bd8ec301a7d6
arlofeirman <arlofeirman@gmail.com>
2018-08-13 23:11:23 -0400

    tests for <AddTodoForm/>

--- test/components/AddTodoForm.spec.tsx
+++ test/components/AddTodoForm.spec.tsx
@@ -12,9 +12,47 @@ describe('<AddTodoForm />', () => {
 
   it('should render', () => {
     const props = { handleSubmit: null }
-    const wrapper = mount(<AddTodoForm {...props} />)
+    const wrapper = shallow(<AddTodoForm {...props} />)
     expect(wrapper).to.be.ok
   })
+
+  it('should render one form', () => {
+    const props = { handleSubmit: null }
+    const wrapper = shallow(<AddTodoForm {...props} />)
+    expect(wrapper.find('form').length).to.equal(1)
+  })
+
+  it('should render one input', () => {
+    const props = { handleSubmit: null }
+    const wrapper = shallow(<AddTodoForm {...props} />)
+    expect(wrapper.find('input').length).to.equal(1)
+  })
+
+  it('input should start empty', () => {
+    const props = { handleSubmit: null }
+
+    const wrapper = shallow(<AddTodoForm {...props} />)
+    expect(wrapper.find('input').props().value).to.equal('')
+  })
+
+  it('input should render button', () => {
+    const props = { handleSubmit: null }
+
+    const wrapper = shallow(<AddTodoForm {...props} />)
+    expect(wrapper.find('button').text()).to.equal('Add todo !')
+  })
+
+  it('should update state', () => {
+    const props = { handleSubmit: null }
+
+    const wrapper = shallow(<AddTodoForm {...props} />)
+    wrapper.instance().updateValue('test')
+
+    expect(wrapper.find('input').props().value).to.equal('test')
+  })
+
+
+
 
 
 })
